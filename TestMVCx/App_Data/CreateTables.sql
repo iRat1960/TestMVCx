d813976ccfc1a6b959646a2b28b63f15e6a4fda1
Айрат <xairat@tatneft.ru>
2019-08-26 11:45:44 +0300

    Скрипт создания таблиц

---
+++ TestMVCx/App_Data/CreateTables.sql
@@ -0,0 +1,22 @@
+﻿CREATE DATABASE [TestDatabase]
+GO
+
+USE [TestDatabase];
+GO
+
+CREATE TABLE [dbo].[Users] (
+    [Id]        INT           IDENTITY (1, 1) NOT NULL,
+    [Name]      NVARCHAR (50) NOT NULL,
+    [DateBirth] DATETIME      NOT NULL,
+    [Gender]    BIT           NOT NULL,
+    [ParentID]  INT           NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+CREATE TABLE [dbo].[Relatives] (
+    [ChildId]  INT NOT NULL,
+    [ParentId] INT NOT NULL,
+    PRIMARY KEY CLUSTERED ([ChildId] ASC),
+    CONSTRAINT [FK_Relatives_Users] FOREIGN KEY ([ParentId]) REFERENCES [dbo].[Users] ([Id])
+);
+GO
